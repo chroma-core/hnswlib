58c3df0a7387bc94bd96a425ef6b6a11b27d6ec3
atroyn <anton.troynikov@gmail.com>
2024-06-27 18:10:48 -0700

    windows...

--- setup.py
+++ setup.py
@@ -109,7 +109,7 @@ class BuildExt(build_ext):
                 opts.append("-fvisibility=hidden")
         elif ct == "msvc":
             opts.append('/DVERSION_INFO=\\"%s\\"' % self.distribution.get_version())
-            opts.append("/std::c++17")
+            opts.append("/std:c++latest")
 
         for ext in self.extensions:
             ext.extra_compile_args.extend(opts)
