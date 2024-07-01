7f51170b4744a98bb26207d3d00389b5f815919e
Luke VanderHart <luke@vanderhart.net>
2024-07-01 17:21:17 -0400

    bump minor version number

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import setuptools
 from setuptools import Extension, setup
 from setuptools.command.build_ext import build_ext
 
-__version__ = "0.7.4"
+__version__ = "0.7.5"
 
 include_dirs = [
     pybind11.get_include(),
